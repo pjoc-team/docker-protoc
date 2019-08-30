f5b5340a192e01ecdaf6ddeb091d51b3580a9f4e
blademainer <blademainer@gmail.com>
2019-08-30 11:50:01 +0800

    fix build

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash -e
 
 source ./variables.sh
+if [ "$RELEASE" == "false" ]; then
+  for build in ${BUILDS[@]}; do
+      tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
+      echo "building ${build} container with tag ${tag}"
+    docker build -t ${tag} \
+          -f Dockerfile \
+          --build-arg grpc=${GRPC_VERSION} \
+          --build-arg grpc_java=${GRPC_JAVA_VERSION} \
+          --target ${build} \
+          .
 
-for build in ${BUILDS[@]}; do
-    tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
-    echo "building ${build} container with tag ${tag}"
-	docker build -t ${tag} \
-        -f Dockerfile \
-        --build-arg grpc=${GRPC_VERSION} \
-        --build-arg grpc_java=${GRPC_JAVA_VERSION} \
-        --target ${build} \
-        .
+      if [ "${LATEST}" = true ]; then
+          echo "setting ${tag} to latest"
+          docker tag ${tag} ${CONTAINER}/${build}:latest
+      fi
+  done
+fi
 
-    if [ "${LATEST}" = true ]; then
-        echo "setting ${tag} to latest"
-        docker tag ${tag} ${CONTAINER}/${build}:latest
-    fi
-done
--- push.sh
+++ push.sh
@@ -1,14 +1,23 @@
 #!/bin/bash -e
 
 source ./variables.sh
+if [ "$RELEASE" == "false" ]; then
+  for build in ${BUILDS[@]}; do
+      tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
+      echo "pushing ${tag}"
+      docker push ${tag}
 
-for build in ${BUILDS[@]}; do
-    tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
-    echo "pushing ${tag}"
-    docker push ${tag}
-
-    if [ "${LATEST}" = true ]; then
-        echo "pushing ${tag} as latest"
-        docker push ${CONTAINER}/${build}:latest
-    fi
-done
+      if [ "${LATEST}" = true ]; then
+          echo "pushing ${tag} as latest"
+          docker push ${CONTAINER}/${build}:latest
+      fi
+  done
+else
+  for build in ${BUILDS[@]}; do
+      dev_tag=${CONTAINER}/${build}:${GRPC_VERSION}_${DEV_VERSION}
+      tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
+      latest_tag=${CONTAINER}/${build}:latest
+      echo "building ${build} container with tag ${tag}"
+      docker pull $DEV_REPO && docker tag ${dev_tag} ${tag} && docker tag ${dev_tag} ${latest_tag} && docker push ${tag} && docker push ${latest_tag};
+  done
+fi
