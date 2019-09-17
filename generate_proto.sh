f0ca66f3feaa94463db807fc123fa9720001bb9a
blademainer <blademainer@gmail.com>
2019-09-17 11:18:43 +0800

    add demo scripts

---
+++ generate_proto.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#set -x
+cur_script_dir="`cd $(dirname $0) && pwd`"
+WORK_HOME="${cur_script_dir}/.."
+IMPORT_HOME="${WORK_HOME}/../../../"
+echo "dirname $WORK_HOME"
+echo "WORK_HOME = $WORK_HOME"
+find $WORK_HOME -name "*.proto" | while read proto; do
+  dir="`dirname $proto`"
+  echo "dir: $dir"
+  docker run --rm -v $dir:/defs -v ${IMPORT_HOME}:/input blademainer/protoc-all:1.23_v0.0.3 -i /defs -i /input -d /defs/ -l go -o /defs --validate-out "lang=go:/defs" --with-docs --with-gateway --lint $addition;
+done
